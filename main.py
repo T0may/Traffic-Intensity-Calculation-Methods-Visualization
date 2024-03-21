0ef0f01fad4fc5bc636a8ebb69e7eda554d66d4d
T0may <273132@student.pwr.edu.pl>
2024-03-21 15:22:24 +0100

    Updated graphic design and implemented basic functionality for updating the TXT file and calculating average

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import font
+from tkinter import font, filedialog, IntVar, StringVar
+from os.path import expanduser
+
 
 #variables
 BG_COLOR = "#606166"
 
+
 # Main window
 window = tk.Tk()
 window.title("Traffic Intensity Calculation Methods Visualization")
-window.geometry("620x420")
+window.geometry("620x520")
 window.resizable(False, False)
 window.configure(bg=BG_COLOR)
 label_font = font.Font(size=13)
 
 
 #Functions
-###########################
+def browse_HandlingTime_file():
+    input_file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("TXT files", "*.txt")], initialdir=expanduser("~") + "/Desktop/")
+    if input_file_path:
+        entry_HandlingTime_path.delete(0, tk.END)
+        entry_HandlingTime_path.insert(0, input_file_path)
+
+def browse_CallIntensity_file():
+    input_file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("TXT files", "*.txt")], initialdir=expanduser("~") + "/Desktop/")
+    if input_file_path:
+        entry_CallIntensity_path.delete(0, tk.END)
+        entry_CallIntensity_path.insert(0, input_file_path)
+
+def generate_chart():
+    input_data = entry_HandlingTime_path.get()
+    with open(input_data, "r") as myfile:
+        time_average(myfile)
+
+def time_average(file):
+    total = 0
+    count = 0
+
+    for line in file:
+        num = float(line)
+        total += num
+        count += 1
+
+    if count > 0:
+        average = total/count
+        print(f"average: {average}")
+
 
 # Choosing method
-button_frame = tk.Frame(window, width=720, height=720)
+button_frame = tk.Frame(window)
 button_frame.configure(bg=BG_COLOR)
 button_frame.pack(pady = 50, padx=30)
 
@@ -34,18 +66,32 @@ method2_button.grid(row=1, column=1, padx=20, pady=5)
 
 
 # Choose the method of inserting data
-input_label_data = tk.Label(button_frame, text="How do you want to import your data?", bg=BG_COLOR, font=label_font)
+
+# Handling time
+input_label_data = tk.Label(button_frame, text="Import 'Handling Time'", bg=BG_COLOR, font=label_font)
 input_label_data.grid(row = 2, pady=(20, 5),  columnspan=3)
 
 input_button = tk.Button(button_frame, text="Insert manually")
 input_button.grid(row=3, padx=20, pady=5)
-file_button = tk.Button(button_frame, text="Choose from file")
+file_button = tk.Button(button_frame, text="Choose from file", command=browse_HandlingTime_file)
 file_button.grid(row=3, column=1, padx=20, pady=5)
 
-entry_path = tk.Entry(button_frame)
-entry_path.grid(row=4, padx=20, columnspan=2)
+entry_HandlingTime_path = tk.Entry(button_frame, width=50, bg="#797a7e")
+entry_HandlingTime_path.grid(row=4, padx=20, columnspan=2)
 
-generate_button = tk.Button(button_frame, text="Generate Chart")
-generate_button.grid(row=5, padx=20, pady=15, columnspan = 2)
+#Call intensity
+input_label_data = tk.Label(button_frame, text="Import 'Call intensity'", bg=BG_COLOR, font=label_font)
+input_label_data.grid(row = 5, pady=(20, 5),  columnspan=3)
+
+input_button = tk.Button(button_frame, text="Insert manually")
+input_button.grid(row=6, padx=20, pady=5)
+file_button = tk.Button(button_frame, text="Choose from file", command=browse_CallIntensity_file)
+file_button.grid(row=6, column=1, padx=20, pady=5)
+
+entry_CallIntensity_path = tk.Entry(button_frame, width=50, bg="#797a7e")
+entry_CallIntensity_path.grid(row=7, padx=20, columnspan=2)
+
+generate_button = tk.Button(button_frame, text="Generate Chart", command=generate_chart)
+generate_button.grid(row=8, padx=20, pady=15, columnspan = 2)
 
 window.mainloop()
